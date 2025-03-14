f2c30668b18e603c8b36532a746a8671cdde6047
KristinaFinck <kristina.finck83@gmail.com>
2025-03-14 23:51:11 +0200

    Updated Pub/Sub implementation in publisher-subscriber.js

--- publisher-subscriber.js
+++ publisher-subscriber.js
@@ -14,41 +14,43 @@
 // setTimeout(function subscriber() {}, 1000)
 // promises.then(function subscriber() {})
 const button = {
-    _subscriber: {
+    _subscribers: {
       click: [],
       focus: [],
     },
     click() {
-      this._subscriber['click'].forEach(subscriber => subscriber())
+      this._subscribers['click'].forEach(subscriber => subscriber())
     },
     focus() {
-      this._subscriber['focus'].forEach(subscriber => subscriber())
+      this._subscribers['focus'].forEach(subscriber => subscriber())
     },
     addEventListener(eventName, subscriber) {
-      this._subscriber[eventName].push(subscriber)
+      this._subscribers[eventName].push(subscriber)
     },
     removeEventListener(eventName, subscriber) {
-      this._subscriber[eventName] = this._subscriber[eventName].filter(sub => sub !== subscriber)
+      this._subscribers[eventName] = this._subscribers[eventName].filter(sub => sub !== subscriber)
     },
   }
-//Task 1
-
- function createNewsChannel {
+// Task 1: News Channel (Pub/Sub)
+function createNewsChannel() {
   return {
-    _subscribers: [], //array of subscribers (functions)
-        subscribe,
-        publish
-    ,
-    subscribe(callback) {
-        this._subscriber.push(callback);// add subscriber
-    },
-    unsubscribe(callback) {
-      this._subscribers = this._subscribers.filter(subscriber => subscriber !== callback) //delete subscriber
-    },
-    publish(news) {
-      this._subscribers.forEach(subscriber => subscriber(news))// send news to users
-    }
-}}
+      _subscribers: [], // Array of subscribers (functions)
+
+      subscribe(callback) {
+          this._subscribers.push(callback); // Add subscriber
+      },
+
+      unsubscribe(callback) {
+          this._subscribers = this._subscribers.filter(subscriber => subscriber !== callback); // Delete subscriber
+      },
+
+      publish(news) {
+          this._subscribers.forEach(subscriber => subscriber(news)); // Send news to users
+      }
+  };
+}
+
+// Using the news channel
 const news = createNewsChannel();
 
 function user1(news) {
@@ -59,20 +61,90 @@ function user2(news) {
   console.log("User2 received:", news);
 }
 
-// Подписываем пользователей
 news.subscribe(user1);
 news.subscribe(user2);
 
-// Отправляем новость
 news.publish("Breaking news: JavaScript is awesome!");
-// Должно вывести:
-// User1 received: Breaking news: JavaScript is awesome!
-// User2 received: Breaking news: JavaScript is awesome!
-
-// Отписываем user1
 news.unsubscribe(user1);
-
-// Отправляем новость снова
 news.publish("New ESNext features are coming!");
-// Должно вывести:
-// User2 received: New ESNext features are coming!
+
+// Task 2: Notification System
+function createNotificationSystem() {
+  return {
+      _subscribers: [], // List of subscribers
+
+      subscribe(callback) {
+          this._subscribers.push(callback); // Add subscriber
+      },
+
+      unsubscribe(callback) {
+          this._subscribers = this._subscribers.filter(sub => sub !== callback); // Remove subscriber
+      },
+
+      notify(message) {
+          this._subscribers.forEach(subscriber => subscriber(message)); // Send notification
+      }
+  };
+}
+
+const notifications = createNotificationSystem();
+
+function aliceNotification(msg) {
+  console.log("Alice received:", msg);
+}
+
+function bobNotification(msg) {
+  console.log("Bob received:", msg);
+}
+
+notifications.subscribe(aliceNotification);
+notifications.subscribe(bobNotification);
+notifications.notify("New message: JavaScript is awesome!");
+notifications.unsubscribe(bobNotification);
+notifications.notify("Another update!");
+
+// Task 3: Event Dispatcher System
+function createEventDispatcher() {
+  return {
+      _events: {}, // Store events and subscribers
+
+      subscribe(eventName, callback) {
+          if (!this._events[eventName]) {
+              this._events[eventName] = [];
+          }
+          this._events[eventName].push(callback);
+      },
+
+      unsubscribe(eventName, callback) {
+          if (this._events[eventName]) {
+              this._events[eventName] = this._events[eventName].filter(sub => sub !== callback);
+          }
+      },
+
+      dispatch(eventName, data) {
+          if (this._events[eventName]) {
+              this._events[eventName].forEach(subscriber => subscriber(data));
+          }
+      }
+  };
+}
+
+// Using the event dispatcher
+const dispatcher = createEventDispatcher();
+
+function onUserLogin(data) {
+  console.log("User logged in:", data.username);
+}
+
+function onUserLogout(data) {
+  console.log("User logged out:", data.username);
+}
+
+dispatcher.subscribe("login", onUserLogin);
+dispatcher.subscribe("logout", onUserLogout);
+
+dispatcher.dispatch("login", { username: "Alice" });  // User logged in: Alice
+dispatcher.dispatch("logout", { username: "Bob" });   // User logged out: Bob
+
+dispatcher.unsubscribe("login", onUserLogin);
+dispatcher.dispatch("login", { username: "Charlie" }); // No output expected
